92b6ebf991b607552f3d2eb39c86aaad30dedcc7
yinuotxie <yinuox@uw.edu>
2022-12-14 01:39:23 -0500

    add more arguments

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from qa import *
 from evaluation import *
 from dataset import *
 from transformers import BertForQuestionAnswering, BertTokenizer
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 from sentence_transformers import SentenceTransformer, CrossEncoder
 import argparse
+import numpy as np
 
 
 # Adding optional argument
@@ -21,7 +22,10 @@ def _parse_args():
                         help='path to dev data')
     parser.add_argument('--tokenizer_path', type=str, default='bert-large-uncased-whole-word-masking-finetuned-squad',
                         help='path to tokenizer')
-
+    parser.add_argument('--model_path', type=str, default='bert-large-uncased-whole-word-masking-finetuned-squad',
+                        help='path to model')
+    parser.add_argument('--retriever', type=str, choices=['lexical', 'semantic'], default='lexical',
+                        help='methods of retriever')
     parser.add_argument('--baseline', default=False, action='store_true', help='run the simple baseline')
     parser.add_argument('--print_examples', default='True', action='store_false', help="Print examples")
     parser.add_argument('--eval', default=False, action='store_true', help='whether to eval the pipeline.')
@@ -32,6 +36,7 @@ def _parse_args():
 
 if __name__ == "__main__":
     args = _parse_args()
+    # assert args.
     print("Loading Dataset")
     train_passages, train_questions, train_answers = load_dataset(args.train_path)
     dev_passages, dev_questions, dev_answers = load_dataset(args.dev_path)
@@ -39,22 +44,26 @@ if __name__ == "__main__":
 
     # load the models
     print("Loading models")
-    # tokenizer = BertTokenizer.from_pretrained(args.tokenizer_path)
-    tokenizer = AutoTokenizer.from_pretrained("deepset/roberta-large-squad2")
-    if args.baseline:
-        print("Using Baseline")
-        reader = BertForQuestionAnswering.from_pretrained('bert-base-uncased')
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path)
+    if args.retriever == 'lexical':
         retriever = LexicalSearch()
     else:
-        print("Using Strong Model")
-        # bi_encoder = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+        bi_encoder = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
         bi_encoder = SentenceTransformer('pinecone/bert-retriever-squad2')
         bi_encoder.max_seq_length = 512
         cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')
-        # reader = BertForQuestionAnswering.from_pretrained('deepset/roberta-large-squad2')
-        reader = AutoModelForQuestionAnswering.from_pretrained("deepset/roberta-large-squad2")
         retriever = PassageRanking(bi_encoder, cross_encoder)
+
     retriever.fit(train_passages + dev_passages)
+
+    if args.baseline:
+        print("Using Baseline")
+        reader = AutoModelForQuestionAnswering.from_pretrained('bert-base-uncased')
+
+    else:
+        print("Using Strong Model")
+        reader = AutoModelForQuestionAnswering.from_pretrained(args.model_path)
+
     print("========================\n")
 
     qa = QuestionAnswering(tokenizer, retriever, reader)
@@ -68,21 +77,24 @@ if __name__ == "__main__":
         print(f'F1 score: {f1_score}\n')
 
     if args.print_examples:
-        sample_questions = ["When did Angelos born?",
-                            "In what university is Angelos studying now?",
-                            "What is Angelos' nationality?",
-                            "What are his scientific interests?",
-                            "What I will do right now?"]
-
-        sample_answers = ["8 April 2001", "University of Athens",
-                          "half Cypriot and half Greek", "Artificial Intelligence",
-                          "stop talking about me"]
+        np.random.seed(5300)
+        idx = np.random.choice(np.arange(len(dev_questions)), 10)
+        sample_questions = np.array(dev_questions)[idx]
+        sample_answers = [[text['text'] for text in ans] for ans in np.array(dev_answers, dtype=object)[idx]]
 
         for question, answer in zip(sample_questions, sample_answers):
             print(f"Question: {question}")
             prediction = qa.question_answer(question, display=True)
-            em_score = exact_match(prediction, answer)
-            f1_score = compute_f1(prediction, answer)
+            em_score = False
+            f1_score = float('-inf')
+            if len(answer) == 0:
+                true_answer = ''
+                em_score = em_score or exact_match(prediction, true_answer)
+                f1_score = max(f1_score, compute_f1(prediction, true_answer))
+            else:
+                for true_answer in answer:
+                    em_score = em_score or exact_match(prediction, true_answer)
+                    f1_score = max(f1_score, compute_f1(prediction, true_answer))
 
             print(f'Prediction: {prediction}')
             print(f'True Answer: {answer}\n')
